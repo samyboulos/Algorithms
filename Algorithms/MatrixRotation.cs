f40e9d9c1f908b44bac97238f456a83f5191648d
Samy Boulos <33524484+samyboulos@users.noreply.github.com>
2018-06-20 11:36:26 -0700

    Update MatrixRotation.cs

--- Algorithms/MatrixRotation.cs
+++ Algorithms/MatrixRotation.cs
@@ -22,36 +22,34 @@ namespace Algorithms
         private void Rotate(int[,] matrix)
         {
             int n = matrix.GetLength(0);
-            int layers = (int)Math.Ceiling((double)n / 2);
+	if (n == 0 || n==1)
+	 return ;
+	 
+	int layers = (int)Math.Ceiling((double)n / 2);
 
-            for (int layer = 0; layer < layers; layer++)
-            {
-                Queue<int> buffer1 = new Queue<int>();
-                Queue<int> buffer2 = new Queue<int>();
-                for (int topx = layer; topx < n - layer; topx++)
-                {
-                    buffer1.Enqueue(matrix[layer, topx]);
-                }
-                for (int righty = layer; righty < n - layer; righty++)
-                {
-                    buffer2.Enqueue(matrix[righty, n - layer - 1]);
-                    matrix[righty, n - layer - 1] = buffer1.Dequeue();
-                }
-                for (int bottomx = n- layer-1; bottomx >= layer; bottomx--)
-                {
-                    buffer1.Enqueue(matrix[n - layer - 1, bottomx]);
-                    matrix[n - layer - 1, bottomx] = buffer2.Dequeue();
-                }
-                for (int lefty = n - layer - 1; lefty >= layer; lefty--)
-                {
-                    buffer2.Enqueue(matrix[lefty, layer]);
-                    matrix[lefty, layer] = buffer1.Dequeue();
-                }
-                for (int topx = layer; topx < n - layer; topx++)
-                {
-                    matrix[layer, topx] = buffer2.Dequeue(); ;
-                }
-            }
+	for (int layer = 0; layer < layers; layer++)
+	{
+		for (int swap = 0; swap < (n - 2 * layer -1); swap++)
+		{
+			int offset = n-layer-1;
+			
+			//top -> right
+			int right= matrix[layer+ swap, offset];
+			matrix[layer+ swap, offset] = matrix[layer, layer + swap];
+			
+			//right -> bottom
+			int bottom = matrix[n-layer-1, offset-swap];
+			matrix[n-layer-1, offset-swap] = right;
+			
+			//bottom -> left
+			int left= matrix[offset-swap, layer];
+			matrix[offset-swap, layer] = bottom;
+			
+			//left-> top
+			matrix[layer, layer+swap] = left;
+		}
+		
+	}
         }
         private void Print (int[,] matrix)
         {
