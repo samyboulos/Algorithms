260e4e6dfc25eafd7856c07da2a545b0c2755aad
Samy Boulos <samy.boulos@hotmail.com>
2018-06-20 21:00:44 -0700

    Add class to BS

--- Algorithms/BinarySearch.cs
+++ Algorithms/BinarySearch.cs
@@ -1,26 +1,32 @@
- public int DoBinarySearch(int[] input, int value)
+namespace Algorithms
+{
+    public class BinarySearch
     {
-        int start =0;
-		int end = input.Length-1;
-	    
-		while(start<=end)
-		{
-		  int median = (start + end) / 2;
-		  
-		  if (input[median] == value)
-          {
-            return median;
-          }
-          else if (input[median] > value)
-          {
-            end = median-1;
-          }
-          else
-          {
-            start= median+1;
-          }
-	    }
-		
-		return -1;
+        public int DoBinarySearch(int[] input, int value)
+        {
+            int start = 0;
+            int end = input.Length - 1;
 
+            while (start <= end)
+            {
+                int median = (start + end) / 2;
+
+                if (input[median] == value)
+                {
+                    return median;
+                }
+                else if (input[median] > value)
+                {
+                    end = median - 1;
+                }
+                else
+                {
+                    start = median + 1;
+                }
+            }
+
+            return -1;
+
+        }
     }
+}
