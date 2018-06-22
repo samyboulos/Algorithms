f8880b595a52e31ff77b1de727909d37117bdbee
Samy Boulos <33524484+samyboulos@users.noreply.github.com>
2018-06-22 10:08:22 -0700

    Create ClimbStairs.cs

---
+++ Algorithms/ClimbStairs.cs
@@ -0,0 +1,19 @@
+public class ClimbStairs
+{
+	public int DoClimbStairs(int n)
+	{
+		if (n == 1) return 1;
+
+		int[] steps = new int[n + 1];
+		steps[0] = 0;
+		steps[1] = 1;
+		steps[2] = 2;
+
+		for (int x = 3; x < n + 1; x++)
+		{
+			steps[x] = steps[x - 1] + steps[x - 2];
+		}
+
+		return steps[n];
+	}
+}
