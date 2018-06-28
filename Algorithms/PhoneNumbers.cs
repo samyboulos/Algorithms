d9be467e80350dd52edfefb81b3a7cb9780e50fc
Samy Boulos <samy.boulos@hotmail.com>
2018-06-28 00:56:38 -0700

    commit

--- Algorithms/PhoneNumbers.cs
+++ Algorithms/PhoneNumbers.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace Algorithms
 {
-    public class PhoneNumberToLetters: Algorithm
+    /// <summary>
+    /// This problem is to change phone numebers to corresponding combination of letters
+    /// </summary>
+
+    public class PhoneNumbers : Algorithm
     {
         private string[] table = new string[] { "", "", "a,b,c", "d,e,f", "g,h,i", "j,k,l", "m,n,o", "p,q,r,s", "t,u,v", "w,x,y,z" };
 
