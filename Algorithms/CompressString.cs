9cd43bc4d1841647971283250dae524daed38a10
Samy Boulos <samy.boulos@hotmail.com>
2018-07-03 17:18:08 -0700

    commit

--- Algorithms/CompressString.cs
+++ Algorithms/CompressString.cs
@@ -1,58 +1,60 @@
-void Main()
+
+
+namespace Algorithms
 {
-	Compress(new char[] {'a','a','b','b','c','c','c'});
-}
+    public class CompressString : IAlgorithm
+    {
 
+        public void Run()
+        {
+            Run(new char[] { 'a','b', 'b', 'b', 'b', 'b' ,'b', 'b', 'b', 'b', 'b', 'b', 'b' });
+        }
 
+        public int Run (char[] chars)
+        {
 
-public int Compress(char[] chars)
-{
+            if (chars.Length == 0)
+                return 0;
 
-	if (chars.Length == 0)
-		return 0;
+            if (chars.Length == 1)
+                return 1;
 
-	if (chars.Length == 1)
-		return 1;
+            int write = 0;
+            int count = 0;
+          
+            for (int read = 0; read < chars.Length; read++)
+            {
+                char current = chars[read];
 
+                char next = (read == chars.Length - 1) ? '0' : chars[read + 1]; // for last iteration set next = '0'
+                count++;
 
-	int write = 0;
-	char previous = '0';
-	int countCurrent = 0;
-	int countPrev =0;
+                if (chars[read] != next) //Last item in current char
+                {
+                    chars[write++] = current; //Write the character
+                    if (count > 1)
+                    {
+                        write += WriteCount(count, write, chars); //Write the count
+                    }
 
-	for (int read = 0; read < chars.Length; read++)
-	{
-		char current = chars[read];
- 		countCurrent++;
-		
-		if (chars[read] != previous || read== chars.Length-1) //Starting new char
-		{
-			//write count of previous
-			if (countPrev > 1)
-			{
-				write += WriteCount(countPrev, write, chars);
-				countPrev = 0;
-			}
-			chars[write] = current; //Write the new character
-			write++; //advance one char
-		}
-		
+                    count = 0;
+                }
 
-		previous = current;
+            }
 
-	}
+            return write;
 
-	return 0;
+        }
 
-}
-
-int WriteCount(int countSeen, int write, char[] chars)
-{
-	//Write the count
-	char[] countChars = countSeen.ToString().ToCharArray();
-	for (int c2 = 0; c2 < countChars.Length; c2++)
-	{
-		chars[write + c2] = countChars[c2];
-	}
-	return countChars.Length; // 
+        int WriteCount(int countSeen, int write, char[] chars)
+        {
+            //Write the count
+            char[] countChars = countSeen.ToString().ToCharArray();
+            for (int c2 = 0; c2 < countChars.Length; c2++)
+            {
+                chars[write + c2] = countChars[c2];
+            }
+            return countChars.Length; // 
+        }
+    }
 }
